6c07a7a9915ee6f23f420a003aa965252fbd25dd
Lukas Pol <lukaspaespol@gmail.com>
2023-01-19 12:14:05 -0300

    feat: create page home

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_30_194241) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_28_180524) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
-
-  create_table "posts", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.string "title"
-    t.string "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
 end
