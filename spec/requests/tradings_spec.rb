7fdf25d7f2cb6041c30dd7695da2be00eaeac54a
Lukas Pol <lukaspaespol@gmail.com>
2023-01-25 10:13:09 -0300

    test(Tradings): create controller #index

---
+++ spec/requests/tradings_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe 'Tradings', type: :request do
+  describe 'GET /index' do
+    context 'when user is logged' do
+      before :each do
+        user = create(:user)
+        create_list(:trading, 3, user:)
+
+        sign_in(user)
+      end
+
+      it 'returns http success' do
+        get tradings_path
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context 'when not logged' do
+      it 'returns http success' do
+        get tradings_path
+        expect(response).to have_http_status(:redirect)
+      end
+    end
+  end
+
+end
