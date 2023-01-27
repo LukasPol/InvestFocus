b07ad8bef2bff71942bbe4760358786543910a1c
Lukas Pol <lukaspaespol@gmail.com>
2023-01-27 16:57:56 -0300

    test(Stock): add column code unique

--- spec/models/stock_spec.rb
+++ spec/models/stock_spec.rb
@@ -4,5 +4,6 @@ RSpec.describe Stock, type: :model do
   describe 'validates' do
     it { should validate_presence_of(:code) }
     it { should validate_length_of(:code) }
+    it { should validate_uniqueness_of(:code) }
   end
 end
