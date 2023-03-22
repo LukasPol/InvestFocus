1806f483d06f9653c4b44ffa4ec61328daf5511d
Lukas Pol <lukaspaespol@gmail.com>
2023-03-22 17:45:34 -0300

    setup: uncomment seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 user = User.create(email: 'test@test.com', password: '123456', password_confirmation: '123456')
 
-# stock = Stock.create(code: 'PETR4')
-#
-# asset = Asset.create(amount: 10, total_invested: 20, average_price: 2, user:, stock:)
-#
-# Trading.create(amount: 10, total_value: 20, value_unit: 2, date: Date.yesterday, user:, stock:, asset:)
+stock = Stock.create(code: 'PETR4')
+
+asset = Asset.create(amount: 10, total_invested: 20, average_price: 2, user:, stock:)
+
+Trading.create(amount: 10, total_value: 20, value_unit: 2, date: Date.yesterday, user:, stock:, asset:)
