2c1631c9981860bcfbd3e0c51eedc9ed918fc308
Lukas Pol <lukaspaespol@gmail.com>
2023-03-23 15:28:28 -0300

    test(Trading): create test controller

--- spec/requests/tradings_spec.rb
+++ spec/requests/tradings_spec.rb
@@ -23,4 +23,65 @@ RSpec.describe 'Tradings', type: :request do
       end
     end
   end
+
+  describe 'GET /new' do
+    context 'when user is logged' do
+      before :each do
+        user = create(:user)
+        sign_in(user)
+      end
+
+      it 'returns http success' do
+        get new_trading_path
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context 'when not logged' do
+      it 'returns http success' do
+        get new_trading_path
+        expect(response).to have_http_status(:redirect)
+      end
+    end
+  end
+
+  describe 'POST /create' do
+    context 'Success' do
+      before :each do
+        user = create(:user)
+        sign_in(user)
+      end
+
+      let(:params) do
+        {
+          trading: {
+            amount: 10,
+            value_unit: 2,
+            total_value: 20,
+            operation_cost: 1,
+            date: Date.yesterday,
+            stock_code: 'TEST9'
+          }
+        }
+      end
+
+      it 'returns http success' do
+        post(tradings_path, params:)
+
+        expect(response).to have_http_status(:created)
+      end
+
+      it 'should create Trading' do
+        expect { post tradings_path, params: }.to(change { Trading.count })
+      end
+
+      it 'should create new Stock' do
+        expect { post tradings_path, params: }.to(change { Stock.count })
+      end
+
+      it 'should create new Asset user' do
+        expect { post tradings_path, params: }.to(change { Asset.count })
+      end
+    end
+  end
 end
