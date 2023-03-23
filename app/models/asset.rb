b7ded27388d6511badb2b061a20366f4a4a36eb5
Lukas Pol <lukaspaespol@gmail.com>
2023-03-23 15:02:49 -0300

    feat(Asset): add field proceed_received

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -4,10 +4,10 @@ class Asset < ApplicationRecord
 
   has_many :tradings, dependent: :restrict_with_error
 
-  validates :stock, :user, :amount, :average_price, :total_invested, presence: true
+  validates :stock, :user, :amount, :average_price, :total_invested, :proceed_received, presence: true
   validates :stock_id, uniqueness: { scope: [:user_id] }
 
-  validates :average_price, :amount, :total_invested, numericality: { greater_than_or_equal_to: 0 }
+  validates :average_price, :amount, :total_invested, :proceed_received, numericality: { greater_than_or_equal_to: 0 }
 
   broadcasts_to ->(asset) { [asset.user, 'assets'] }, inserts_by: :prepend
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_21_194258) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_23_174546) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_21_194258) do
     t.uuid "stock_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "proceed_received", default: 0.0
     t.index ["stock_id", "user_id"], name: "index_assets_on_stock_id_and_user_id", unique: true
     t.index ["stock_id"], name: "index_assets_on_stock_id"
     t.index ["user_id"], name: "index_assets_on_user_id"
